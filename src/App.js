8eb3d857a87cd03614d055281fc249f6b62dfa5d
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-04 19:25:33 +0100

    Created entry point for ZipSearch component in render function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import ZipSearch from './components/ZipSearch'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello World
+        <ZipSearch/>
       </header>
     </div>
   );

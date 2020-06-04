1c96fdd34dc384a80e3b8691ec44dae1fb8ade73
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-04 20:17:35 +0100

    Added implementation to ZipSearch and CityCard

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,8 +1,24 @@
-import React, { Component } from 'React'
+import React, { Component } from "react";
 
 class CityCard extends Component {
-    render() {
-        return <></>
-    }
+  render() {
+    return (
+      <div className="zipcode-card style=width: 18rem;">
+          <div className="card">
+        <div className="card-header">
+          {this.props.city + ", " + this.props.state}
+        </div>
+        <div className="card-body">
+          <ul>
+            <li>State: {this.props.state}</li>
+            <li>Location: {"("+this.props.lat + ", " + this.props.long+")"}</li>
+            <li>Population (estimated): {this.props.population}</li>
+            <li>Total Wages: {this.props.totalwages}</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default CityCard;
--- src/components/ZipSearch.js
+++ src/components/ZipSearch.js
@@ -1,18 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Proptypes from "prop-types";
+import CityCard from "./CityCard";
+import axios from "axios"
 
 class ZipSearch extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "",
-            zipcodes: [],
-        };
-    }
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+      zipcodes: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  fetchZipData = (e) => {
+      const url = `http://ctp-zip-api.herokuapp.com/zip/${this.state.value}`
+    axios.get(url)
+      .then((response) => {
+        const data = response.data;
+        this.setState({ zipcodes: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {  
+    const zipcodeCards = () =>
+      this.state.zipcodes.map((zipcode) => {
         return (
-            <></>
+          <CityCard
+            key={zipcode.RecordNumber}
+            city={zipcode.City}
+            state={zipcode.State}
+            lat={zipcode.Lat}
+            long={zipcode.Long}
+            population={zipcode.EstimatedPopulation}
+            totalwages={zipcode.TotalWages}
+          />
         );
-    }
+      });
+
+    return (
+      <>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              onClick={() => {
+                this.fetchZipData();
+              }}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className="zipcodes">{zipcodeCards()}</div>
+      </>
+    );
+  }
 }
+
+ZipSearch.propTypes = {
+  value: Proptypes.string,
+  zipcodes: Proptypes.object,
+};
 
 export default ZipSearch;
